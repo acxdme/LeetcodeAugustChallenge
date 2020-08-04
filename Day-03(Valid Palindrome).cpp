effe8ed339c220d3baebd21ced5e6a45fd4bde0a
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-08-04 18:00:07 +0530

    Update Day-03(Valid Palindrome).cpp

--- Day-03(Valid Palindrome).cpp
+++ Day-03(Valid Palindrome).cpp
@@ -30,3 +30,25 @@ public:
     }
         
 };
+
+/*
+Second Approach : Use of two pointers but this time  not taking any extra space to recreate the string.
+Time complexity : O(n) 
+Space complexity :O(1)
+*/
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length() <= 1) return true;
+        int st = 0, en = s.length()-1;
+        while(st < en){
+            while(st < en && !isalnum(s[st])) st++;   //isalnum() checks if the character is alphaNumeric or not.
+            while(st < en && !isalnum(s[en])) en--;
+            if(st < en && tolower(s[st]) != tolower(s[en])) return false;
+            st++;
+            en--;
+        }
+        return true;
+    }
+};
+

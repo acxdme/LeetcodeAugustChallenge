a53b42c7cd79114c46e34b92232f22aa02ed832d
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-08-25 22:13:34 +0530

    Update Day-24(SumOfLeftLeaves).cpp

--- Day-24(SumOfLeftLeaves).cpp
+++ Day-24(SumOfLeftLeaves).cpp
@@ -39,3 +39,34 @@ public:
         return sum;
     }
 };
+
+
+/*
+space optimized dfs approach to calculate the left leaves sum in a tree.
+*/
+
+class Solution {
+  public:
+  int sum=0;
+ void leftSum(TreeNode* root, bool flag)
+ {
+      if(root==NULL)
+          return;
+     
+     if(flag==true and root->left==NULL and root->right==NULL)
+         sum+=root->val;
+     
+     leftSum(root->left,true);
+     leftSum(root->right,false);
+ }
+    
+ int sumOfLeftLeaves(TreeNode* root) 
+ {
+     if(root==NULL)
+         return 0;
+     leftSum(root,false);
+     return sum;
+ }
+        
+    
+};
